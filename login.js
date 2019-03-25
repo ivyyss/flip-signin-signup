fa4e6d905fb4d349ae876307b99398ca27980d9c
ivyyss <nameyss144@163.com>
2019-03-25 16:40:09 +0800

    flip-login

--- login.js
+++ login.js
@@ -6,27 +6,15 @@ function $$(selector){
 }
 
 $('header .login span').onclick=function(){
-    $('.flip-module').style.display = 'block'
+    $('.flip-module').style.display = 'block' 
 }
 
-
-// console.log($$('.flip-module .tabs .loginTab'))
-// $$('.flip-module .tabs .loginTab').forEach(function(node){
-// node.onclick = function(){
-//     $('.flip-module').classList.remove('register')
-//     $('.flip-module').classList.add('login')    
-// }
-// })
-// $$('.flip-module .tabs .registerTab').forEach(function(node){
-//     node.onclick = function(){
-//         $('.flip-module').classList.remove('login')
-//         $('.flip-module').classList.add('register')    
-// }
-// })
-
-
-
-
+$$('.closeTab').forEach(function(b){
+    b.onclick = function(){
+        $('.flip-module').style.display = 'none'
+        return false;
+    }
+})
 
 $$('.loginTab').forEach(function(a){
     a.onclick = function(){
@@ -41,3 +29,4 @@ $('.flip-module').classList.add('register');
 $('.flip-module').classList.remove('login');
     }
 })
+
